e7672971a68fd8d510820143a90674b3120057c9
Ramona Snider <41329897+rlsnider@users.noreply.github.com>
2022-11-22 18:29:21 -0800

    delet route done

--- server/src/router.js
+++ server/src/router.js
@@ -4,11 +4,12 @@ const router = express.Router();
 const readTodosRoute = require('./routes/readTodosRoute');
 const createTodoRoute = require('./routes/createTodoRoute');
 const updateTodoRoute = require('./routes/updateTodoRoute');
+const deleteTodoRoute = require('./routes/deleteTodoRoute')
 
  router.get('/todos', readTodosRoute);
  router.post('/todos', createTodoRoute);
  router.put('/todos/:id', updateTodoRoute);
-
+ router.delete('/todos/:id', deleteTodoRoute);   
 
 // router.put('/todos/:id', (req, res) => { 
 //     console.log('todo put route')
