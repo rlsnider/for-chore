ecaca8c7f09c3a5185fd8ef8548bb7f6beb5134d
Ramona Snider <41329897+rlsnider@users.noreply.github.com>
2022-11-27 12:40:32 -0800

    TodoForm and Welcome on login dashboard view

---
+++ frontend/src/components/TodoForm.jsx
@@ -0,0 +1,10 @@
+import { useState } from 'react'
+import { useSelector, useDispatch } from 'react-redux'
+
+function TodoForm() {
+  return (
+    <div>TodoForm</div>
+  )
+}
+
+export default TodoForm
--- frontend/src/views/Dashboard.jsx
+++ frontend/src/views/Dashboard.jsx
@@ -1,6 +1,7 @@
 import { useEffect } from 'react'
 import {useNavigate} from 'react-router-dom'
 import {useSelector} from 'react-redux'
+import TodoForm from '../components/TodoForm'
 
 function Dashboard() {
   const navigate = useNavigate()
@@ -13,7 +14,13 @@ function Dashboard() {
     }
   }, [user, navigate])
   return (
-    <div>Dashboard</div>
+   <>
+   <section className="heading">
+      <h1>Welcome {user && user.name}</h1>
+      <p>Todos List</p>
+    </section>
+    <TodoForm />
+   </>
   )
 }
 
