00ee5f8f19c941e4c1040d1995d80f77ec774c4f
Ramona Snider <41329897+rlsnider@users.noreply.github.com>
2022-11-27 12:51:26 -0800

    can add text to todo list on form

--- frontend/src/components/TodoForm.jsx
+++ frontend/src/components/TodoForm.jsx
@@ -2,8 +2,28 @@ import { useState } from 'react'
 import { useSelector, useDispatch } from 'react-redux'
 
 function TodoForm() {
+    const [text, setText] = useState('')
+
+    const onSubmit = e => {
+        e.preventDefault()
+    }
+
   return (
-    <div>TodoForm</div>
+ <section className='form'>
+    <form onSubmit={onSubmit}>
+        <div className="form-group">
+            <label htmlFor="text">Todo</label>
+            <input 
+            type='text'
+            name='text'
+            id='text'
+            value={text}
+            onChange={(e) => setText(e.target.value)}
+            />
+        </div>
+
+    </form>
+ </section>
   )
 }
 
